794500c9bb279694e4b4b8262cf05b589ad7121e
joeegin <3318329726@qq.com>
2025-06-27 21:41:47 +0800

    添加注释

--- rpc/Message.h
+++ rpc/Message.h
@@ -22,4 +22,29 @@ struct VoteResponse {
     NLOHMANN_DEFINE_TYPE_INTRUSIVE(VoteResponse, term, voteGranted)
 };
 
+//添加raft同步日志
+struct LogEntry {
+    int term;
+    std::string command;//客户端请求，例如put x 5
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(LogEntry, term, command)
+};
+
+struct AppendEntries {
+    int term;
+    int leaderId;
+    int prevLogIndex;//此次复制的日志的前一条日志索引
+    int prevLogTerm;//上一条日志的Term
+    std::vector<LogEntry> entries;
+    int leaderCommit;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(AppendEntries, term, leaderId, prevLogIndex,prevLogTerm,entries,leaderCommit)
+};
+
+struct AppendResponse {
+    int term;
+    bool success;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(AppendResponse, term, success)
+};
 #endif //MESSAGE_H
