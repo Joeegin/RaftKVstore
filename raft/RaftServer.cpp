7192b594dfcda55ce55da74bffc608e3ab269117
joeegin <3318329726@qq.com>
2025-06-27 12:21:32 +0800

    添加Boost/Asio网络库支持

---
+++ raft/RaftServer.cpp
@@ -0,0 +1,116 @@
+//
+// Created by 乔益金 on 25-6-26.
+//
+
+#include "RaftServer.h"
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <nlohmann/json.hpp>
+#include <thread>
+#include <memory>
+
+using boost::asio::ip::tcp;
+using json = nlohmann::json;
+
+
+RaftServer::RaftServer(boost::asio::io_context &io_context, int id, int port, const std::vector<std::pair<int, int> > &peers)
+    :_io_context(io_context),_id(id),_port(port),
+    _acceptor(io_context,tcp::endpoint(tcp::v4(),port)){
+
+    //保存其他节点id和端口
+    for(const auto & [peerid,peerport] :peers) {
+        if (peerid != id) {
+            _peerPorts[peerid]=peerport;
+        }
+    }
+    // 创建 RaftNode，传入发送投票的函数
+    _node = std::make_shared<RaftNode>(id, peers.size(), [this](int from, int to) {
+        sendVoteRequest(to);
+    });
+}
+
+void RaftServer::run() {
+    std::cout << "[Node " << _id << "] listening on port " << _port << std::endl;
+    StartAcceptor();
+
+    // 模拟 tick（心跳+选举）
+    std::thread([this]() {
+        while (true) {
+            _node->tick();
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    }).detach();
+
+    _io_context.run(); // Boost Asio事件循环
+}
+
+void RaftServer::StartAcceptor() {
+    auto socket = std::make_shared<tcp::socket>(_io_context);
+    _acceptor.async_accept(*socket,[this,socket](boost::system::error_code ec) {
+        if (!ec) {
+            handleConnection(socket);
+        }
+        StartAcceptor(); // 再次监听下一个连接
+    });
+}
+
+void RaftServer::handleConnection(std::shared_ptr<tcp::socket> socket) {
+    auto buffer=std::make_shared<boost::asio::streambuf>();
+    boost::asio::async_read_until(*socket,*buffer,'\n',
+        [this,socket,buffer](boost::system::error_code ec, std::size_t ) {
+            if (!ec) {
+                std::istream is(buffer.get());
+                std::string line;
+                std::getline(is, line);
+                handleMessage(line, socket);
+            }
+        });
+}
+
+void RaftServer::handleMessage(const std::string &msg, std::shared_ptr<tcp::socket> socket) {
+    try {
+        auto message = json::parse(msg);//反序列化
+        if (message.contains("type") && message["type"]=="RequestVote") {
+            RequestVote req=message["data"].get<RequestVote>();//反序列化
+            std::cout << "[Node " << _id << "] received vote request from Node " << req.candidateId << std::endl;
+            _node->receiveVoteRequest(req.term,req.candidateId);
+            VoteResponse resp{req.term,true};
+            json respMsg={
+                {"type","RequestVoteResponse"},
+                    {"data",resp}
+            };
+             std::string response=respMsg.dump()+"\n";//序列化响应并且在结尾加上换行
+            boost::asio::async_write(*socket,boost::asio::buffer(response),[](auto,auto) {
+
+            });
+        }
+    }catch (std::exception &e) {
+        std::cerr << "[Node " << _id << "] Error parsing message: " << e.what() << std::endl;
+    }
+}
+
+void RaftServer::sendVoteRequest(int peerId) {
+    auto it = _peerPorts.find(peerId);
+    if (it ==_peerPorts.end()){return;}
+
+    auto peerport=it->second;
+    auto socket = std::make_shared<tcp::socket>(_io_context);
+    auto endpoint=tcp::endpoint(tcp::v4(),peerport);
+
+    socket->async_connect(endpoint,[this,socket,peerId](boost::system::error_code ec) {
+        if (!ec) {
+            RequestVote req{_node->getCurrentTerm(),_id};
+            json reqMsg={
+                {"type","RequestVote"},
+                {"data",req}
+            };
+            std::string message = reqMsg.dump() + "\n";
+
+            boost::asio::async_write(*socket, boost::asio::buffer(message), [](auto, auto) {});
+
+        }
+    });
+
+
+}
---
+++ raft/RaftServer.h
@@ -0,0 +1,39 @@
+//
+// Created by 乔益金 on 25-6-26.
+//
+
+#ifndef RAFTSERVER_H
+#define RAFTSERVER_H
+
+#include <boost/asio.hpp>
+#include <thread>
+#include <memory>
+#include <unordered_map>
+#include "../raft/RaftNode.h"
+#include "../rpc/Message.h"
+
+
+class RaftServer {
+public:
+    RaftServer(boost::asio::io_context& io_context,int ip,int port, const std::vector<std::pair<int,int>>& peers);
+    void run();
+
+private:
+    void StartAcceptor();
+    void handleConnection(std::shared_ptr<boost::asio::ip::tcp::socket> socket);
+    void handleMessage(const std::string& msg, std::shared_ptr<boost::asio::ip::tcp::socket> socket);
+    void sendVoteRequest(int peerId);
+
+    int _id;
+    int _port;
+    std::unordered_map<int,int> _peerPorts;
+
+    boost::asio::io_context& _io_context;
+    boost::asio::ip::tcp::acceptor _acceptor;
+
+    std::shared_ptr<RaftNode> _node;
+};
+
+
+
+#endif //RAFTSERVER_H
